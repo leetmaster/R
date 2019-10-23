94a91cb165a76434bbe25dd5f94bec2b0165cff3
Ruben Garcia <rbn@leetmaster.local>
2019-10-22 22:27:30 -0500

    Mejorando material

--- ejemplo.R
+++ ejemplo.R
@@ -2,10 +2,6 @@ df <- read.csv(url("https://datos.cdmx.gob.mx/explore/dataset/contador-ciclistas
 
 id <- c('Año', 'Alcaldia', 'Cursos')
 
-apellido <- c('Muñoz', 'Perez')
-
-persona <- data.frame(nombre, apellido)
-
 summary(df)
 
 str(df)
@@ -24,6 +20,10 @@ persona$apellido
 
 df <- read.csv(url("https://datos.cdmx.gob.mx/explore/dataset/historico-de-resultados-de-los-cursos-de-capacitacion-a-locatarios-de-los-mercad/download/?format=csv&timezone=America/Mexico_City&use_labels_for_header=true"), sep = ";", col.names = (id),  stringsAsFactors = TRUE)
 
+# En este caso el año es una variable cualitativa, lo indicamos con la función levels
+
+df$Año <- factor(df$Año, levels = c("2013","2014", "2015", "2016", "2017", "2018", "2019"),
+                    labels =c("2013","2014", "2015", "2016", "2017", "2018", "2019"))
 str(df)
 
 summary(df)
@@ -32,5 +32,7 @@ hist(df$Cursos)
 
 plot(df$Alcaldia, df$Cursos)
 
-abline(df[Año])
+head(df)
+tail(df)
 
+df$Año = 2019
