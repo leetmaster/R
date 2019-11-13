b6bf14eab63e428fa2ada967f9062ade0845c138
Ruben Garcia <leetmaster@users.noreply.github.com>
2019-11-12 20:31:35 -0600

    Update ejemplo.R
    Mejorando las gráficas

--- ejemplo.R
+++ ejemplo.R
@@ -10,27 +10,31 @@ getwd()
 library(dplyr)
 
 # Vamos a importar un set de datos (data set) pero primero:
-# Declarar un vector para darle nombre a las columnas
 
-id <- c('Año', 'Alcaldia', 'Cursos')
-
-# Declarar un vectro para darle nombre a las filas
-alcaldias <- c("Álvaro Obregón",
+# Declarar un vector que contenga los nombres de las alcaldÃ­as de la CDMX 
+# Lo usaremos para darle nombre a las filas
+alcaldias <- c("Ãlvaro ObregÃ³n",
                 "Azcapotzalco",
-                "Benito Juárez",
-                "Coyoacán",
+                "Benito JuÃ¡rez",
+                "CoyoacÃ¡n",
                 "Cuajimalpa",
-                "Cuauhtémoc",
+                "CuauhtÃ©moc",
                 "Gustavo A. Madero",
                 "Iztacalco",
                 "Iztapalapa",
                 "Magdalena Contreras",
                 "Miguel Hidalgo",
                 "Milpa Alta",
-                "Tláhuac",
+                "TlÃ¡huac",
                 "Tlalpan",
                 "Venustiano Carranza",
                 "Xochimilco")
+
+# Ordenamos el vector de manera ascendente 
+alcaldias <- sort(alcaldias, decreasing = FALSE)
+
+# Declarar un vector para darle nombre a las columnas
+id <- c('AÃ±o', 'Alcaldia', 'Cursos')
 
 # Importamos el dataset historico-de-capacitacion.csv usando los nombres de columna que definimos con col.names = (id)
 # Observen que son necesarios dobles diagonales regulares //
@@ -41,24 +45,34 @@ df <- read.csv("C://Users//josegarcia//R//historico-de-capacitacion.csv", sep = 
 
 str(df)
 
-# En este caso el año es una variable cualitativa, lo indicamos con la función levels
+# En este caso el aÃ±o es una variable cualitativa, lo indicamos con la funciÃ³n levels
 
-df$Año <- factor(df$Año, levels = c("2013","2014", "2015", "2016", "2017", "2018", "2019"))
+df$AÃ±o <- factor(df$AÃ±o, levels = c("2013","2014", "2015", "2016", "2017", "2018", "2019"))
 
-# Las alcaldías tienen errores de codificación, puedo cambiar los nombres
+# Las alcaldÃ­as tienen errores de codificaciÃ³n, puedo cambiar los nombres
 levels(df$Alcaldia) <- alcaldias 
 
-# Visualizo sólo los nombres de las alcaldías
+# Visualizo sÃ³lo los nombres de las alcaldÃ­as
 df$Alcaldia
 
-#¿Cómo puedo visualizar las otras columnas?
+#Â¿CÃ³mo puedo visualizar las otras columnas?
 
 # Mostrar el resumen de la tabla
 summary(df)
 
-# ¿Qué valores puedes observar?
+# Â¿QuÃ© valores puedes observar?
 
 # R me permite graficar los datos
+#install.packages("ggplot2")
+library("ggplot2")
+
+ggplot(df, aes(x=Cursos)) + geom_histogram(binwidth=5, color="white", fill="navy")
+
+ggplot(df, aes(x=Alcaldia,y=Cursos)) + geom_boxplot(color="black", fill="red")
+
+ggplot(df, aes(x=Alcaldia,y=Cursos)) + geom_point(color="green")
+
+
 plot(df)
 hist(df$Cursos)
 
@@ -70,32 +84,32 @@ median(df$Cursos)
 # Obtener la varianza de la columna Cursos
 var(df$Cursos)
 
-# Obtener la desviación estándar de la columna Cursos
+# Obtener la desviaciÃ³n estÃ¡ndar de la columna Cursos
 sd(df$Cursos)
 
-# ¿De qué otra manera puedo obtener la desviación estándar?
+# Â¿De quÃ© otra manera puedo obtener la desviaciÃ³n estÃ¡ndar?
 
 # var(df$Cursos) ** .5 == sd(df$Cursos)
 
-# Ordenar la tabla con base en la variable año
+# Ordenar la tabla con base en la variable aÃ±o
 
-arrange(df, Año)
+arrange(df, AÃ±o)
 
-# Guardar en una variable únicamente la columna 2, para ello utilizamos su índice
+# Guardar en una variable Ãºnicamente la columna 2, para ello utilizamos su Ã­ndice
 
 alcaldias <- df[, 2]
 
 print(alcaldias)
 
-# Agrupar la columna Cursos por año y guardarla en una variable
-# Usamos %>% para separar el código en líneas para hacerlo más legible
-tabla_cursos_por_año <- df %>%
-  group_by(Año) %>%
+# Agrupar la columna Cursos por aÃ±o y guardarla en una variable
+# Usamos %>% para separar el cÃ³digo en lÃ­neas para hacerlo mÃ¡s legible
+tabla_cursos_por_aÃ±o <- df %>%
+  group_by(AÃ±o) %>%
   summarise(mean(Cursos))
 
 # probar cambiar mean por sum en la sentencia anterior
-# ¿Cuál es el resultado?
-tabla_cursos_por_año
+# Â¿CuÃ¡l es el resultado?
+tabla_cursos_por_aÃ±o
 
 
 # Extraer el 50% de los datos de forma aleatoria con reemplazo.
@@ -104,21 +118,21 @@ df %>%
   sample_frac(0.50, rep=T)
 
 # Muestreo aleatorio simple con reemplazo: 
-# Después de que un elemento ha sido seleccionado del marco de la muestra se devuelve y es elegible para ser seleccionado nuevamente.
+# DespuÃ©s de que un elemento ha sido seleccionado del marco de la muestra se devuelve y es elegible para ser seleccionado nuevamente.
 # Extraer 10 datos de forma aleatoria sin reemplazo.
 
 df %>% 
   sample_n(10)
 
-# Mostrar aquellas alcaldías que impartieron 20 cursos o más
+# Mostrar aquellas alcaldÃ­as que impartieron 20 cursos o mÃ¡s
 filter(df, Cursos >=20)
 
-# Mostrar las alcaldías que dieron más de 10 cursos en 2019
+# Mostrar las alcaldÃ­as que dieron mÃ¡s de 10 cursos en 2019
 
-filter (select(df, Año, Alcaldia, Cursos), Año == 2019, Cursos > 10)
+filter (select(df, AÃ±o, Alcaldia, Cursos), AÃ±o == 2019, Cursos > 10)
 
-# Mostrar los cursos impartidos por la alcaldía Benito Juárez
+# Mostrar los cursos impartidos por la alcaldÃ­a Benito JuÃ¡rez
 
-filter (select(df, Año, Alcaldia, Cursos), Alcaldia == "Benito Juárez")
+filter (select(df, AÃ±o, Alcaldia, Cursos), Alcaldia == "Benito JuÃ¡rez")
 
-# ¿Puedo mostrar los cursos impartidos por otra alcaldía?
+# Â¿Puedo mostrar los cursos impartidos por otra alcaldÃ­a?
